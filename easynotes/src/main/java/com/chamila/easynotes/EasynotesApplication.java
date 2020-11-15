780818ff546536bc6528b960fcb949331734d0e5
cjchamila <cjchamila@gmail.com>
2020-11-15 10:08:12 +0530

    update main class

--- easynotes/src/main/java/com/chamila/easynotes/EasynotesApplication.java
+++ easynotes/src/main/java/com/chamila/easynotes/EasynotesApplication.java
@@ -4,7 +4,6 @@ import java.util.Iterator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
